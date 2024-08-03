2fdde93c6b3559cb760a89fc8408bc8b923b7d50
wishket-ysj <sjyoon@wishket.com>
2024-08-03 14:47:35 +0900

    feat: 테이블 migrate 해주는 fixture 추가

--- conftest.py
+++ conftest.py
@@ -1,15 +1,24 @@
+import pytest
+from sqlmodel import SQLModel
+
 from db import init_db_engine, init_async_session
 
 
+@pytest.fixture
 async def db_session():
     engine = init_db_engine()
     async with engine.connect() as conn:
         await conn.begin()
         await conn.begin_nested()
 
+        await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
         session_class = init_async_session(conn)
         async with session_class() as session:
             yield session
         await conn.rollback()
+
+        await conn.run_sync(SQLModel.metadata.drop_all)
 
     await engine.dispose()
