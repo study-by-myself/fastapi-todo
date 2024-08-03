9641c2ea6259f78b6e11109113051e06d5e809ec
blingblin-g <luvbliny@gmail.com>
2024-08-03 15:41:29 +0900

    feat: base model

--- models.py
+++ models.py
@@ -1,10 +1,26 @@
-from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+from sqlmodel import SQLModel as _SQLModel, Field, func, DateTime, Column
+
+
+class SQLModel(_SQLModel):
+    id: int = Field(default=None, primary_key=True)
+    created: datetime = Field(
+        sa_column=Column(DateTime, server_default=func.now()),
+    )
+    updated: datetime = Field(
+        sa_column=Column(DateTime, server_default=func.now(), server_onupdate=func.now()),
+    )
+    deleted: datetime = Field(nullable=True)
 
 
 class User(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
     name: str = Field(max_length=16)
     username: str = Field(max_length=16, unique=True)
     password: str
     tmi: str
 
+
+class Category(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(max_length=16)
